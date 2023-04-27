6be9f987aa5738f471cf01dae0b7a2246b5f6d9e
Trey Shelton <trey@basecampcodingacademy.org>
2023-04-26 21:59:01 -0500

    my solution

--- trey/index.js
+++ trey/index.js
@@ -1,2 +1,15 @@
 fetch("../planets/planets.json").then(function(response) {
+   response.json().then(function(json) {
+      const destination = document.getElementById("destination");
+      let index = 0;
+      destination.addEventListener("click", function(){
+         destination.innerHTML = `
+            <div>
+               <h3>Planet ${json[index].name}</h3>
+               <img src=${json[index].image} height=250></img>
+           </div>
+        `;
+                 index = (index + 1) % json.length;
+      });
+   });
 });
